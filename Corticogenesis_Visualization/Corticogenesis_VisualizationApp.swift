368691c5fd9acbffede54615b0e7428d0bfdc308
Cong Le <longchike@gmail.com>
2025-06-28 22:19:24 -0700

    the setup

---
+++ Corticogenesis_Visualization/Corticogenesis_VisualizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Corticogenesis_VisualizationApp.swift
+//  Corticogenesis_Visualization
+//
+//  Created by Cong Le on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Corticogenesis_VisualizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Corticogenesis_VisualizationUITests/Corticogenesis_VisualizationUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Corticogenesis_VisualizationUITestsLaunchTests.swift
+//  Corticogenesis_VisualizationUITests
+//
+//  Created by Cong Le on 6/28/25.
+//
+
+import XCTest
+
+final class Corticogenesis_VisualizationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
